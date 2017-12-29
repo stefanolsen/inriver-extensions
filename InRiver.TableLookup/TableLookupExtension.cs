4eea0d29457c1c68542ba77b4115013f9540089f
Stefan Holm Olsen <stefan.holm.olsen@gmail.com>
2017-12-29 17:13:13 +0100

    Rename project.

---
+++ InRiver.TableLookup/TableLookupExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using inRiver.Remoting.Extension;
+using inRiver.Remoting.Extension.Interface;
+using inRiver.Remoting.Objects;
+
+namespace InRiver.TableLookupExtension
+{
+    public class TableLookupExtension : IEntityListener
+    {
+        public inRiverContext Context { get; set; }
+
+        public Dictionary<string, string> DefaultSettings { get; }
+
+        public string Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EntityCreated(int entityId)
+        {
+        }
+
+        public void EntityUpdated(int entityId, string[] fields)
+        {
+        }
+
+        public void EntityDeleted(Entity deletedEntity)
+        {
+        }
+
+        public void EntityLocked(int entityId)
+        {
+        }
+
+        public void EntityUnlocked(int entityId)
+        {
+        }
+
+        public void EntityFieldSetUpdated(int entityId, string fieldSetId)
+        {
+        }
+
+        public void EntityCommentAdded(int entityId, int commentId)
+        {
+        }
+
+        public void EntitySpecificationFieldAdded(int entityId, string fieldName)
+        {
+        }
+
+        public void EntitySpecificationFieldUpdated(int entityId, string fieldName)
+        {
+        }
+    }
+}
