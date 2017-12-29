55ef1a28c91028afd3cd2a57c0ab339402ca07b8
Stefan Holm Olsen <stefan.holm.olsen@gmail.com>
2017-12-29 16:50:46 +0100

    Add project files.

---
+++ InRiver.TableLookupExtension/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InRiver.TableLookupExtension
+{
+    public class Class1
+    {
+    }
+}
