c48436a9675f809e231fdb719178ccc30160fa61
Stefan Holm Olsen <stefan.holm.olsen@gmail.com>
2018-01-05 16:41:28 +0100

    Add basic code for testing image analyzer.

---
+++ InRiver.ImageAnalyzer.Tests/ImageAnalyzerExtensionTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using inRiver.Remoting.Extension;
+using inRiver.Remoting.Extension.Interface;
+using inRiver.Remoting.Log;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace InRiver.ImageAnalyzer.Tests
+{
+    public class ImageAnalyzerExtensionTest
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public ImageAnalyzerExtensionTest(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void TestExtensionTestMethod()
+        {
+            IEntityListener extension = CreateExtension();
+
+            string result = extension.Test();
+            extension.Context.Log(LogLevel.Debug, result);
+        }
+
+        private ImageAnalyzerExtension CreateExtension()
+        {
+            var extension = new ImageAnalyzerExtension();
+            extension.Context = new inRiverContext(null, new XUnitLogger(_outputHelper))
+            {
+                Settings = new Dictionary<string, string>
+                {
+                    { "ITEM_ENTITY_TYPE_ID", "Item" },
+                    { "PRODUCT_ENTITY_TYPE_ID", "Product" },
+                    { "PRODUCTITEM_LINK_TYPE_ID", "ProductItem" },
+                    { "BRAND_FIELD_TYPE_ID", "ProductBrand" },
+                    { "COLORID_FIELD_TYPE_ID", "ItemColorId" },
+                    { "TABLE_NAME", "ColorGroups" }
+                }
+            };
+
+            return extension;
+        }
+    }
+}
---
+++ InRiver.ImageAnalyzer/AnalyzeImageResponse.cs
@@ -0,0 +1,66 @@
+﻿namespace InRiver.ImageAnalyzer
+{
+    public class AnalyzeImageResponse
+    {
+        public Adult Adult { get; set; }
+
+        public Color Color { get; set; }
+
+        public Description Description { get; set; }
+
+        public TagItem[] Tags { get; set; }
+    }
+
+    public class Adult
+    {
+        public bool IsAdultContent { get; set; }
+
+        public bool IsRacyContent { get; set; }
+
+        public decimal AdultScore { get; set; }
+
+        public decimal RacyScore { get; set; }
+    }
+
+    public class Description
+    {
+        public string[] Tags { get; set; }
+
+        public Caption[] Captions { get; set; }
+    }
+
+    public class Caption
+    {
+        public decimal Confidence { get; set; }
+
+        public string Text { get; set; }
+    }
+
+    public class Color
+    {
+        public string DominantColorBackground { get; set; }
+        public string DominantColorForeground { get; set; }
+
+        public string[] DominantColors { get; set; }
+
+        public string AccentColor { get; set; }
+
+        public bool IsBWImg { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string Format { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+    }
+
+    public class TagItem
+    {
+        public string Name { get; set; }
+
+        public decimal Confidence { get; set; }
+    }
+}
