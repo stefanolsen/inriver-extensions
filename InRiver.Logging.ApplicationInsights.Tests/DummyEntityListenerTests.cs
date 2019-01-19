b29752306f37d6cf652e3c5f56f027eb5a7dc421
Stefan Holm Olsen <stefan@stefanolsen.com>
2019-01-19 19:57:18 +0400

    Add test project for logging extension.

---
+++ InRiver.Logging.ApplicationInsights.Tests/DummyEntityListenerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using inRiver.Remoting.Extension;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace InRiver.Logging.ApplicationInsights.Tests
+{
+    public class DummyEntityListenerTests
+    {
+        private readonly DummyEntityListener _subject;
+
+        public DummyEntityListenerTests(
+            ITestOutputHelper testOutputHelper)
+        {
+            _subject = new DummyEntityListener
+            {
+                Context = new inRiverContext(
+                    null,
+                    new XUnitLogger(testOutputHelper))
+            };
+        }
+
+        [Fact]
+        public void RunTestMethod()
+        {
+            // Arrange
+            _subject.Context.Settings = new Dictionary<string, string>
+            {
+                {SettingsConstants.DisableTelemetry, "FALSE"},
+                {SettingsConstants.InstrumentationKey, "REPLACE ME"}
+            };
+
+            // Act
+            string result = _subject.Test();
+
+            // Assert
+            Assert.StartsWith("SUCCESS:", result);
+        }
+    }
+}
